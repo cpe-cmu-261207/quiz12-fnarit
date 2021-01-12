e8d86fe4eea28437f309250dbcc97eba0cc3446e
fnarit <naritsararat@gmail.com>
2021-01-12 08:46:09 +0700

    1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,25 @@
 //your code here!
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<div class='level'>
+        <div class='level-item'>
+            <a class='subtitle'>Number</a>
+            <input id='input-number' placeholder='Insert number..' class='input' style='width: 200px'></input>
+            <button id='button-compute' class='button is-primary'>Modify Student ID</button>
+        </div>
+    </div>
+
+<script>
+
+function myFunction() {
+  document.getElementById("button-compute");
+}
+</script>
+
+</body>
+</html>
+
+
